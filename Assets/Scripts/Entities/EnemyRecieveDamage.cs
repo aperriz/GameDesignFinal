a072e38e2b8b8cd408c8ae561802e9144ac4fb1a
aperriz <alec.perrizo@gmail.com>
2024-04-15 15:39:44 -0500

    random

--- Assets/Scripts/Entities/EnemyRecieveDamage.cs
+++ Assets/Scripts/Entities/EnemyRecieveDamage.cs
@@ -13,7 +13,9 @@ public class EnemyRecieveDamage : MonoBehaviour
     public AudioClip audioClip;
     public float speed;
     protected float tempSpeed;
-
+    GameObject player;
+    [SerializeField]
+    int xForce = 2, yForce = 2;
 
     // Start is called before the first frame update
     void Awake()
@@ -23,12 +25,21 @@ public class EnemyRecieveDamage : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         audioSource.clip = audioClip;
         tempSpeed = speed;
+        player = GameObject.Find("Player");
     }
 
     public void DealDamage(int damage)
     {
         if (!animator.GetBool("Invincible"))
         {
+            xForce = -xForce;
+            yForce = -yForce;
+
+            if(gameObject.GetComponent<Rigidbody2D>().totalForce.x >= 0 && gameObject.GetComponent<Rigidbody2D>().totalForce.y >= 0)
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(xForce, yForce), ForceMode2D.Impulse);
+            }
+            
             audioSource.Play();
             health -= damage;
             animator.SetBool("Hurt", true);
@@ -39,7 +50,7 @@ public class EnemyRecieveDamage : MonoBehaviour
 
     protected void CheckOverheal()
     {
-        if(health > maxHealth)
+        if (health > maxHealth)
         {
             health = maxHealth;
         }
@@ -48,7 +59,8 @@ public class EnemyRecieveDamage : MonoBehaviour
 
     protected void CheckDeath()
     {
-        if (health <= 0){
+        if (health <= 0)
+        {
             Debug.Log("Dead");
             animator.SetBool("Dead", true);
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
@@ -65,8 +77,8 @@ public class EnemyRecieveDamage : MonoBehaviour
     {
         animator.SetBool("Hurt", false);
         animator.SetBool("Invincible", false);
-        CheckDeath() ;
-        
+        CheckDeath();
+
     }
 
     protected void AttackDone()
